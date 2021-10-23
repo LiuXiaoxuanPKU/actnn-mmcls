ef06e1ccdcca7d216af091c1bc1e33ad18126f13
LiuXiaoxuanPKU <lilyliupku@gmail.com>
2021-10-23 23:04:40 +0000

    vgg config

---
+++ configs/actnn/vgg11bn_b64x4_imagenet_new.py
@@ -0,0 +1,39 @@
+_base_ = [
+    '../_base_/models/vgg11bn.py',
+    '../_base_/datasets/imagenet_bs32_pil_resize.py',
+    '../_base_/schedules/imagenet_bs256.py',
+    '../_base_/default_runtime.py'
+]
+actnn = True
+data = dict(
+    samples_per_gpu=64, # 64*4 = 256
+    workers_per_gpu=2,
+)
+
+check_gradient = True
+# # do not update weights to check gradient
+# optimizer_config = dict(grad_clip=None, update_interval=10000)
+
+log_config = dict(
+    interval=100,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(
+            type='WandbLoggerHook',
+            init_kwargs=dict(
+                project='classification',
+                entity='actnn',
+                name='vgg11bn_b64x4_imagenet',
+            )
+        )
+    ]
+)
+
+layer_id = 79
+custom_hooks = [
+    dict(
+        type="CheckGradientHook",
+        minibatch=True,
+        interval=1
+    )
+]
