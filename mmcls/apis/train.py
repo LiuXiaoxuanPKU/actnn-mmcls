wrong way to calculate gradient var

mmcls/apis/train.py
@@ -98,7 +98,8 @@ def train_model(
         )
     else:
         if device == "cuda":
-            model = MMDataParallel(model.cuda(cfg.gpu_ids[0]), device_ids=cfg.gpu_ids)
+            model = MMDataParallel(model.cuda(
+                cfg.gpu_ids[0]), device_ids=cfg.gpu_ids)
         elif device == "cpu":
             model = model.cpu()
         else:
@@ -108,7 +109,8 @@ def train_model(
     optimizer = build_optimizer(model, cfg.optimizer)
 
     if cfg.get("runner") is None:
-        cfg.runner = {"type": "EpochBasedRunner", "max_epochs": cfg.total_epochs}
+        cfg.runner = {"type": "EpochBasedRunner",
+                      "max_epochs": cfg.total_epochs}
         warnings.warn(
             "config is now expected to have a `runner` section, "
             "please set `runner` in your config.",
@@ -179,7 +181,8 @@ def train_model(
     if cfg.actnn:
         import actnn
 
-        controller = actnn.controller.Controller()
+        controller = actnn.controller.Controller(
+            default_bit=cfg.bit, auto_prec=cfg.auto_prec, check_error=True)
         controller.filter_tensors(runner.model.named_parameters())
         # actnn.ops.filter_tensors(runner.model.named_buffers())
         controller.filter_tensors(runner.optimizer.state.items())
@@ -187,30 +190,13 @@ def train_model(
 
         def pack_hook(x):
             r = controller.quantize(x)
-            if cfg.check_gradient:
-                set_random_seed(0, True)
             return r
 
         def unpack_hook(x):
             r = controller.dequantize(x)
-            if cfg.check_gradient:
-                set_random_seed(0, True)
             return r
 
         with torch.autograd.graph.saved_tensors_hooks(pack_hook, unpack_hook):
             runner.run(data_loaders, cfg.workflow)
     else:
-        if cfg.check_gradient:
-
-            def pack_hook(x):
-                set_random_seed(0, True)
-                return x
-
-            def unpack_hook(x):
-                set_random_seed(0, True)
-                return x
-
-            with torch.autograd.graph.saved_tensors_hooks(pack_hook, unpack_hook):
-                runner.run(data_loaders, cfg.workflow)
-        else:
-            runner.run(data_loaders, cfg.workflow)
+        runner.run(data_loaders, cfg.workflow)

mmcls/core/hooks/check_gradient_hook.py
@@ -71,53 +71,76 @@ class CheckGradientHook(Hook):
         minibatch (bool): If current run is a minibatch
     """
 
-    def __init__(self, minibatch, interval=1):
+    def __init__(self, interval=1):
         self.interval = interval
-        self.minibatch = minibatch
         self.gradient = None
-        self.gradients = []
+        self.start_cal_var = False
+        self.mean_grad = None
+        self.total_var = 0
 
     def after_train_iter(self, runner):
         if self.every_n_iters(runner, 1):
             gradient = None
             model = (
-                runner.model.module if is_module_wrapper(runner.model) else runner.model
+                runner.model.module if is_module_wrapper(
+                    runner.model) else runner.model
             )
+            # gradient for current iteration
             for param in model.parameters():
                 if param.grad is not None:
                     cur_param = param.grad.reshape(1, -1)
+                    if torch.isnan(param.grad).any():
+                        print("Param grad conatins nan")
+                        print(param)
+                        exit(0)
                     if gradient is None:
                         gradient = cur_param
                     else:
                         gradient = torch.cat((gradient, cur_param), 1)
-            if self.gradient is None:
-                self.gradient = gradient
-            else:
-                self.gradient += gradient
-            self.gradients.append(gradient)
+
+            # calculate mean gradient
+            if not self.start_cal_var:
+                if self.gradient is None:
+                    self.gradient = gradient
+                else:
+                    self.gradient += gradient
+
+            # calculate gradient variance
+            if self.start_cal_var:
+                var = (gradient - self.mean_grad) ** 2
+                self.total_var += var.sum()
 
         if self.every_n_iters(runner, self.interval):
-            mean_grad = self.gradient / self.interval
-            var = None
-            for i in range(self.interval):
-                if var is None:
-                    var = (self.gradients[i] - mean_grad) ** 2
-                else:
-                    var += (self.gradients[i] - mean_grad) ** 2
-            print("Gradient Variance", var.sum() / self.interval)
+            if not self.start_cal_var:
+                self.mean_grad = self.gradient / self.interval
+                self.start_cal_var = True
+            else:
+                print("Gradient Variance", self.total_var / self.interval)
+                self.total_var = 0
+
+        # if self.every_n_iters(runner, self.interval):
+            # mean_grad = self.gradient / self.interval
+            # var = None
+            # for i in range(self.interval):
+            #     if var is None:
+            #         var = (self.gradients[i] - mean_grad) ** 2
+            #     else:
+            #         var += (self.gradients[i] - mean_grad) ** 2
+            # print("Gradient Variance", var.sum() / self.interval)
+            # self.gradients = []
+            # self.gradient = None
             # exit(0)
 
-            if runner.actnn:
-                prefix = "actnn"
-            else:
-                prefix = "no_actnn"
-            if self.minibatch:
-                prefix += "_minibatch"
-            else:
-                assert self.interval == 1
-                prefix += "_batch"
-            filename = "gradients/%s_gradient.p" % (prefix)
-            print("Save gradient to ", filename)
-            with open(filename, "wb") as f:
-                pickle.dump(self.gradient, f)
-            exit(0)
+            # if runner.actnn:
+            #     prefix = "actnn"
+            # else:
+            #     prefix = "no_actnn"
+            # if self.minibatch:
+            #     prefix += "_minibatch"
+            # else:
+            #     assert self.interval == 1
+            #     prefix += "_batch"
+            # filename = "gradients/%s_gradient.p" % (prefix)
+            # print("Save gradient to ", filename)
+            # with open(filename, "wb") as f:
+            #     pickle.dump(self.gradient, f)